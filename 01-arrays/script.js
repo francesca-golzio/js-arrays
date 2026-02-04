7bde766fe6d2a3595a01755f95c93b4516b8bb03
Francesca Golzio <francescagolzio@gmail.com>
2026-02-04 16:21:27 +0100

    verify teachers.length isn't empty

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,6 +10,7 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
+
 const fourthTeacher = teachers[3];
 //console.log(fourthTeacher);
 
@@ -28,21 +29,23 @@ const lastTeacher = teachers.pop();
 //console.log(lastTeacher);
 
 
-
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
+
 //console.log(teachers);
 const firstTeacher = teachers.shift();
 //console.log(firstTeacher);
 
+
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+
 //console.log(teachers);
 /*salva in const          |  operazione richiesta */ 
 const teacherPushedToEnd = teachers.push('Vanessa')
 //console.log(teachers);
 
 
-
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+
 //console.log(teachers);
 /* salva in const          |  operazione richiesta */ 
 const teacherAddedToStart = teachers.unshift('Sarah')
@@ -51,10 +54,14 @@ const teacherAddedToStart = teachers.unshift('Sarah')
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
+
 //console.log(teachers);
 const lewisIndex = teachers.indexOf('Lewis');
 //console.log(lewisIndex);
 
+
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+
+const isTeachersEmpty = (teachers.length === 0);
+//console.log(isTeachersEmpty);
