b18bbca9f0138589e5646d5d40861a4551d0ee65
Francesca Golzio <francescagolzio@gmail.com>
2026-02-04 15:48:18 +0100

    remove 1st teacher

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -11,7 +11,7 @@ const teachers = [
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
-console.log(fourthTeacher);
+//console.log(fourthTeacher);
 
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
@@ -30,8 +30,11 @@ const lastTeacher = teachers.pop();
 
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+//console.log(teachers);
+const firstTeacher = teachers.shift();
+//console.log(firstTeacher);
 
+//console.log(teachers);
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
